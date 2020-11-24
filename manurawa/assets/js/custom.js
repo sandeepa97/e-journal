f1dbf1370f9a7f27a4664891da39e04a54a0a492
sandeepa97 <lbsandeepa@gmail.com>
2020-11-24 20:39:43 +0530

    mail fixed

---
+++ manurawa/assets/js/custom.js
@@ -0,0 +1,6 @@
+//  =======================================================
+// * Web Name: Manurawa 2020
+// * Web Version: 1.0
+// * Developer: Sandeepa Lokubaranige | www.sandeepa.net
+// * Owner: SLLCHRM | www.sllchrm.lk
+// ======================================================== 
