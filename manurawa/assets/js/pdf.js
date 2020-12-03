d599ebd91922fbe9c59ed0eef84fe0687e6b8f0c
sandeepa97 <lbsandeepa@gmail.com>
2020-12-04 02:38:11 +0530

    pdf viewer new 3

---
+++ manurawa/assets/js/pdf.js
@@ -0,0 +1,10 @@
+//  =======================================================
+// * Web Name: Manurawa 2020
+// * Web Version: 1.0
+// * Developer: Sandeepa Lokubaranige | www.sandeepa.net
+// * Owner: SLLCHRM | www.sllchrm.lk
+// ======================================================== 
+
+$(document).ready(function() {
+    $(".pdf-container").pdfviewer();
+});
